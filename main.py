94df1eff816f7d00f02e4eb2b5a08169ffbcf4f8
Student <humanita@ualberta.ca>
2022-01-10 20:43:30 -0500

    step 10

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import requests
 
-google_homepage = requests.get('http://www.google.com'))
+google_homepage = requests.get('http://www.google.com')
+
+repo_code = requests.get('https://raw.githubusercontent.com/humanita-cmput404/lab1/main/main.py'
+).text
+print(repo_code)
