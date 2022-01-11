18e6687b00e5858ff7e2eb91f13ce5cb05f01568
Student <humanita@ualberta.ca>
2022-01-10 20:55:27 -0500

    removed google get request

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 import requests
 
-google_homepage = requests.get('http://www.google.com')
-
-repo_code = requests.get('https://raw.githubusercontent.com/humanita-cmput404/lab1/main/main.py'
-).text
+repo_code = requests.get('https://raw.githubusercontent.com/humanita-cmput404/lab1/main/main.py').text
 print(repo_code)
